09f9f38903649a98444d90abfb3311629693729e
mightydhiyamatra <mightydhiyamatra@yahoo.co.id>
2019-08-19 16:06:57 +0700

    add Router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
-function App() {
+import Hello from './Pages/Hello';
+import Profile from './Pages/Profile';
+import NavBar from './Components/NavBar';
+
+const App =  () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <Route
+        path={'/hello'}
+        render={() => {
+          return <Hello age={29} />;
+        }}
+      />
+      <Route path={'/profile'} component={Profile} />
+    </Router>
   );
-}
+};
 
 export default App;
